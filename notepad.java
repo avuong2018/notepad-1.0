c09c0029c6822fd61e0948128a9e25677252c5b8
Anh Vuong <avuong2018@gmail.com>
2016-12-01 11:08:08 -0600

    Update notepad.java

--- notepad.java
+++ notepad.java
@@ -1,7 +1,26 @@
- import java.io.*;
- import java.awt.*;
- import java.awt.event.*;
- import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
  
  public class notepad extends JFrame implements ActionListener {
  	private JFileChooser fc = new JFileChooser();
